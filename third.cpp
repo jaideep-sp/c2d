1844edd93a3cecc70fa3cde9cc01b10ae3178842
jaideep-sp <81135835+jaideep-sp@users.noreply.github.com>
2021-09-19 08:25:46 +0530

    Add files via upload

---
+++ third.cpp
@@ -0,0 +1,110 @@
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+
+
+int main()
+{
+	int n; cin>>n;
+	int town[n][2];
+	int m1,m2;
+	for (int i = 0; i < n; i++)
+	{
+		cin>>m1>>m2;
+		town[i][0]=m1;
+		town[i][1]=m2;
+	}
+	
+    int ysum(0);
+	for (int i = 0; i < n; i++)
+	{
+		ysum += town[i][0];
+	}
+	int xsum[n];
+
+    for (int i = 0; i < n; i++)
+	{ 
+		xsum[i] = town[i][0]+town[i][1];  
+	}
+
+	int si = sizeof(xsum) / sizeof(xsum[0]);
+	sort(xsum, xsum + si,greater<int>());
+	
+
+    int k(0),tsum(0),camp(0);
+
+	while (tsum <= ysum)
+	{
+		tsum += xsum[k];
+		k++;
+		camp++;
+	}
+    cout<<camp;
+
+	return 0;
+}
